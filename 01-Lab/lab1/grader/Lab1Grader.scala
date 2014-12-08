0c04e7ce205f6b0083291654df5f024059dddaa6
Dillon Drenzek <dr.enzek@gmail.com>
2014-12-07 20:32:01 -0700

    removed extra files

--- 01-Lab/lab1/grader/Lab1Grader.scala
+++
@@ -1,5 +0,0 @@
-object Lab1Grader {
-  def main(args: Array[String]) {
-    (new Lab1Grading).execute(stats = true)
-  }
-}
--- lab2 2/Lab2.md
+++
@@ -1,1 +0,0 @@
-Replace with your writeup using Markdown, just like we did in Lab 1.
